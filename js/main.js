df64fcde0cdd67dbb1302804d676b95f3ffdb108
Constelium.xyz <constelium.xyz@gmail.com>
2023-11-19 17:19:07 +0100

    update

---
+++ js/main.js
@@ -0,0 +1,7 @@
+
+// document
+//   .getElementsByClassName("our-canvases-btn")[0]
+//   .addEventListener("click", function () {
+//     console.log("Our canvases button clicked");
+//     window.location.href = "/productpage1/ourcanvases.html";
+//   });
