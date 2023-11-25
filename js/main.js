9bd97dfa95347359b014da9ad754be3d480c0497
Constelium.xyz <constelium.xyz@gmail.com>
2023-11-25 19:41:37 +0100

    update

--- js/main.js
+++
@@ -1,7 +0,0 @@
-
-// document
-//   .getElementsByClassName("our-canvases-btn")[0]
-//   .addEventListener("click", function () {
-//     console.log("Our canvases button clicked");
-//     window.location.href = "/productpage1/ourcanvases.html";
-//   });
---
+++ popup.js/account.js
@@ -0,0 +1,9 @@
+// Fonction pour ouvrir la pop-up
+function openPopup() {
+  document.getElementById("accountPopup").style.display = "flex";
+}
+
+// Fonction pour fermer la pop-up
+function closePopup() {
+  document.getElementById("accountPopup").style.display = "none";
+}
